ab7cdd7d99070e3c29b5ad6bb1fbf98efd8a5a6b
Sergei Lukianov <me@slukjanov.name>
2023-04-03 09:19:53 -0700

    Add support for printing sprint summary

--- pkg/mdbook/gh.go
+++ pkg/mdbook/gh.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"html"
 	"io"
-	"log"
 	"os"
 	"strings"
 
@@ -49,9 +48,8 @@ func (p *GitHubProjectPreprocessor) Process(in io.Reader, out io.Writer) error {
 			end := strings.Index(source[start:], TOKEN_CLOSE) + start
 			if end-start > 3 && end < len(source) {
 				item := source[start : end-1]
-				log.Println("Processing github item:", item)
 
-				formatted, err := p.formatGitHubItem(item)
+				formatted, err := p.processExpr(item)
 				if err != nil {
 					return errors.Wrapf(err, "error formatting github item %s", item)
 				}
@@ -96,14 +94,19 @@ func (p *GitHubProjectPreprocessor) readBook(in io.Reader) (string, error) {
 	return string(bookBytes), nil
 }
 
-func (p *GitHubProjectPreprocessor) formatGitHubItem(ref string) (string, error) {
+func (p *GitHubProjectPreprocessor) processExpr(ref string) (string, error) {
 	parts := strings.Split(ref, "#")
-	if len(parts) != 2 {
-		return "", errors.Errorf("invalid github item ref, should be repo#number")
+	if len(parts) == 2 {
+		if parts[0] == "$summary" {
+			return p.formatSummary(parts[1])
+		} else {
+			return p.formatGitHubItem(parts[0], parts[1])
+		}
 	}
-	repo := parts[0]
-	number := parts[1]
+	return "", errors.Errorf("invalid expression: should be '$summary#sprint' or github item ref like 'repo#number'")
+}
 
+func (p *GitHubProjectPreprocessor) formatGitHubItem(repo string, number string) (string, error) {
 	if item, ok := p.items[fmt.Sprintf("/%s/%s/issues/%s", p.org, repo, number)]; ok {
 		return fmt.Sprintf(" <a href='%s' title='%s'>`%s#%s: %s (%.1f)`</a> ",
 			item.URL(), html.EscapeString(item.Title()), repo, number, item.Status(), item.Estimate()), nil
@@ -111,4 +114,87 @@ func (p *GitHubProjectPreprocessor) formatGitHubItem(ref string) (string, error)
 
 	return fmt.Sprintf(" <a href='https://github.com/githedgehog/%s/issues/%s'>`%s#%s`</a> ",
 		repo, number, repo, number), nil
+}
+
+var usersMapping = map[string]string{
+	"Frostman":    "Sergei L",
+	"mheese":      "Marcus",
+	"mkoperator":  "Mikhail",
+	"sonoble":     "Steve",
+	"amitlimaye":  "Amit",
+	"sergeymatov": "Sergey M",
+}
+
+func (p *GitHubProjectPreprocessor) formatAssignee(name string) string {
+	if name == "" {
+		return "unassigned"
+	}
+
+	if mapped, ok := usersMapping[name]; ok {
+		return mapped
+	}
+
+	return name
+}
+
+var statuses = []string{
+	"🆕 New",
+	"📋 Backlog",
+	"🏗 In progress",
+	"👀 In review",
+	"✅ Done",
+}
+
+func (p *GitHubProjectPreprocessor) formatSummary(sprint string) (string, error) {
+	stats := map[string][]github.ProjectItem{}
+
+	for _, item := range p.items {
+		if item.Sprint() != sprint {
+			continue
+		}
+
+		name := p.formatAssignee(item.Assignee())
+		stats[name] = append(stats[name], item)
+	}
+
+	summary := strings.Builder{}
+	summary.WriteString("<table>")
+
+	summary.WriteString("<thead>")
+	summary.WriteString("<tr>")
+	summary.WriteString("<th>Name</th>")
+	for _, status := range statuses {
+		summary.WriteString(fmt.Sprintf("<th>%s</th>", status))
+	}
+	summary.WriteString("<th>Total</th>")
+	summary.WriteString("</tr>")
+	summary.WriteString("</thead>")
+
+	summary.WriteString("<tbody>")
+	for name, items := range stats {
+		summary.WriteString("<tr>")
+		summary.WriteString(fmt.Sprintf("<td>%s</td>", name))
+
+		total := 0.0
+		for _, status := range statuses {
+			estimate := 0.0
+			for _, item := range items {
+				if item.Status() != status {
+					continue
+				}
+
+				estimate += item.Estimate()
+			}
+			total += estimate
+			summary.WriteString(fmt.Sprintf("<td>%.1f</td>", estimate))
+		}
+
+		summary.WriteString(fmt.Sprintf("<td>%.1f</td>", total))
+		summary.WriteString("</tr>")
+	}
+
+	summary.WriteString("</tbody>")
+	summary.WriteString("</table>")
+
+	return summary.String(), nil
 }
